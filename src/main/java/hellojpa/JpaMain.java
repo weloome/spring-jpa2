d08ca41bda168ceff0ce35dcccb4a0f45e761b92
danbi <leedanbiii@naver.com>
2024-01-30 17:46:43 +0900

    Member 등록

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,31 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(3L);
+            member.setName("hello3");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
